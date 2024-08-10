a8bacdecce39b9aa6bf411c4432056c2cfafcf45
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-08-10 16:14:09 +0200

    mycommits

--- myclient/myclient/settings.py
+++ myclient/myclient/settings.py
@@ -85,13 +85,17 @@ WSGI_APPLICATION = 'myclient.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dan_backend',
+        'USER': 'database101',
+         'PASSWORD': 'masizaCREATIVES!3',
+        'HOST': 'dan-datab.c9q80s0oet0v.eu-north-1.rds.amazonaws.com',  
+        'PORT': '5432',  # default port for PostgreSQL
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
