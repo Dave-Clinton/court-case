812e4e72c1d72839fd0c270b6eb2f83545e67de8
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-08-11 16:43:05 +0200

    whatsappu

--- myclient/myclient/settings.py
+++ myclient/myclient/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*' ,'cout-order.onrender.com']
 
 # Set the SITE_URL based on the DEBUG value
 if DEBUG:
-    SITE_URL = ['http://13.60.180.5:8000/']
+    SITE_URL = 'http://13.60.180.5:8000/'
 else:
     SITE_URL = 'http://13.60.180.5:8000/'
 
