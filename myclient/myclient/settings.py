c01859ef7477837099c7e45690b0c8551e862036
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-08-11 05:11:45 +0200

    settings.py

--- myclient/myclient/settings.py
+++ myclient/myclient/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)rpq4jvhujouna8(je@+r_6n3#$uv@ezky@6f38t%!^f0od#9o
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True # Ensure this is set to False in production
+DEBUG = False# Ensure this is set to False in production
 import os
 # Set allowed hosts for development
 
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*' ,'cout-order.onrender.com']
 if DEBUG:
     SITE_URL = 'http://127.0.0.1:8000'
 else:
-    SITE_URL = 'https://cout-order.onrender.com'
+    SITE_URL = 'http://13.60.180.5:8000/'
 
 print(f'DEBUG is set to {DEBUG}. Site URL is set to {SITE_URL}')
 
